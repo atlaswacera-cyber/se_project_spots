64192a50a2f2b56526bd8adb1fe6cd4b864e25c5
atlaswacera-cyber <atlaswacera@gmail.com>
2025-12-26 15:09:11 -0600

    update project

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
-modal.classList.add("modal_is-opened");
+const editProfilebtn = document.querySelector(".edit__profile-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editCloseBtn = editProfileModal.querySelector(".edit__close-btn");
+
+const newPostBtn = document.querySelector(".profile__newpost-close-btn");
+const NewPostModal = document.querySelector("#newpost-modal");
+const newPostCloseBtn = NewPostModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+editCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  NewPostModal.classList.add("modal_is-opened");
+});
+newPostCloseBtn.addEventListener("click", function () {
+  NewPostModal.classList.remove("modal_is-opened");
+});
