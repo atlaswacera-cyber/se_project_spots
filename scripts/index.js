a093b8dde319ad23d82a8b5fa9b36180d529606d
atlaswacera-cyber <atlaswacera@gmail.com>
2025-12-26 15:23:50 -0600

    update project

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,23 @@
-const editProfilebtn = document.querySelector(".edit__profile-btn");
+const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editCloseBtn = editProfileModal.querySelector(".edit__close-btn");
+const editCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__newpost-close-btn");
-const NewPostModal = document.querySelector("#newpost-modal");
-const newPostCloseBtn = NewPostModal.querySelector(".modal__close-btn");
+const newPostBtn = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
 });
+
 editCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
 newPostBtn.addEventListener("click", function () {
-  NewPostModal.classList.add("modal_is-opened");
+  newPostModal.classList.add("modal_is-opened");
 });
+
 newPostCloseBtn.addEventListener("click", function () {
-  NewPostModal.classList.remove("modal_is-opened");
+  newPostModal.classList.remove("modal_is-opened");
 });
