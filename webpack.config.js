ec68c01f53ced6a357fa3e71a36069a05dfc77ee
atlaswacera-cyber <atlaswacera@gmail.com>
2026-04-16 12:26:57 -0500

    Add webpack build and GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/pages/index.js",
+  entry: "./pages/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ttf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      favicon: "./src/images/favicon.ico",
+      template: "./vendor/index.html",
+      favicon: "./images/favicon.ico",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "images", to: "images" }],
     }),
   ],
 };
